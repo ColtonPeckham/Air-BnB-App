4f60f8ecfb6eb99057e5e863073af498f8dfcb06
Cole Price <colevart@gmail.com>
2022-11-21 17:01:14 -0500

    usersseedsr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,46 @@
 class UsersController < ApplicationController
   def index
+    @user = User.all
+    render template: "users/index"
   end
 
   def new
+    @user = User.new
+    render template: "users/new"
   end
 
   def create
-  end
-
-  def show
+    @user = User.new(
+      name: params[:user][:name],
+      email: params[:user][:email],
+      password: params[:user][:password],
+      password_confirmation: params[:user][:password_confirmation],
+      image: params[:user][:image],
+    )
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to "/"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @user = User.find_by(id: params[:id])
+    render template: "users/edit"
   end
 
   def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.image = params[:user][:image]
+    @user.save
+    redirect_to "/users"
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to "/login"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
