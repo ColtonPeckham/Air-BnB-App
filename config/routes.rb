92ed29852de4f707c450ea40157f47dcc4554bcd
Colton Peckham <coltonpeckham@gmail.com>
2022-11-22 15:48:33 -0600

    added Room controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :rooms
+
   get "/signup" => "users#new"
   post "/users" => "users#create"
-  
+
   get "/login" => "sessions#new"
   post "/sessions" => "sessions#create"
   get "/logout" => "sessions#destroy"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,9 @@
 
 # User.create(name: "Colton", email: "colton@example.com", image: "placeholder", password: "password1", password_confirmation: "password1")
 # User.create(name: "Steve", email: "steve@example.com", image: "placeholder", password: "password", password_confirmation: "password")
+
+#Room.create(user_id: 1, address: "2014 Forest Hills Drive", city: "Fayetteville", state: "North Carolina", price: 100.00, description: "J Cole was here", home_type: "residential", room_type:"house", total_occupancy: 2, total_bedrooms: 3, total_bathrooms: 2)
+
+#Room.create(user_id: 2, address: "5219 Walker Ave", city: "Lincoln", state: "Nebraska", price: 80.00, description: "Apartment complex in the North central part of Lincoln", home_type: "apartment", room_type:"Air BnB", total_occupancy: 1, total_bedrooms: 1, total_bathrooms: 1)
+
+#Room.create(user_id: 3, address: "1234 El Rancho Road", city: "Sidney", state: "Nebraska", price: 160.00, description: "a place along side of the 17th hole on the golf course", home_type: "residential", room_type:"house", total_occupancy: 3, total_bedrooms: 4, total_bathrooms: 2)
