1041c75c1c691ee880e5cfa981944546ffaf80bf
StevenMetz <stevenm.codes@gmail.com>
2022-11-22 13:50:55 -0600

    views created to make a new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,11 +5,28 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @reviews = Review.new(
+      reservation_id: params[:reservation_id],
+      rating: params[:rating],
+      comment: params[:comment],
+    )
+    @reviews.save
+    redirect_to "/"
   end
 
   def update
+    @reviews = Review.new(
+      reservation_id: params[:reservation_id],
+      rating: params[:rating],
+      comment: params[:comment],
+    )
+    @reviews.save
+
+    # redirect_to "/rooms" need to make it redirect to room reviews.
   end
 
   def edit
+    @reviews = Review.find_by(id: params[:id])
+    render template: "reviews/edit"
   end
 end
