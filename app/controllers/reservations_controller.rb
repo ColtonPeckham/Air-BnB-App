c5e397fb9c92bb35035e1aa0d1cbfc66e66512ff
Cole Price <colevart@gmail.com>
2022-11-22 16:48:32 -0500

    reservations controller/routes/views

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,52 @@
+class ReservationsController < ApplicationController
+  def index
+    @reservation = Reservation.all
+    render template: "reservations/index"
+  end
+
+  def show
+    @reservation = Reservation.find_by(id: params[:id])
+    render template: "reservations/show"
+  end
+
+  def new
+    @reservation = Reservation.new
+    render template: "reservations/new"
+  end
+
+  def create
+    @reservation = Reservation.new(
+      user_id: params[:reservation][:user_id],
+      room_id: params[:reservation][:room_id],
+      start_date: params[:reservation][:start_date],
+      end_date: params[:reservation][:end_date],
+      price: params[:reservation][:price],
+      total: params[:reservation][:total],
+    )
+    @reservation.save
+    redirect_to "/reservations"
+  end
+
+  def update
+    @reservation = Reservation.find_by(id: params[:id])
+    @reservation.user_id = params[:reservation][:user_id]
+    @reservation.room_id = params[:reservation][:room_id]
+    @reservation.start_date = params[:reservation][:start_date]
+    @reservation.end_date = params[:reservation][:end_date]
+    @reservation.price = params[:reservation][:price]
+    @reservation.total = params[:reservation][:total]
+    @reservation.save
+    redirect_to "/reservations"
+  end
+
+  def edit
+    @reservation = Reservation.find_by(id: params[:id])
+    render template: "reservations/edit"
+  end
+
+  def destroy
+    @reservation = Reservation.find_by(id: params[:id])
+    @reservation.destroy
+    redirect_to "/reservations", status: :see_other
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get "/signup" => "users#new"
   post "/users" => "users#create"
-  
+
   get "/login" => "sessions#new"
   post "/sessions" => "sessions#create"
   get "/logout" => "sessions#destroy"
+
+  resources :reservations
 end
